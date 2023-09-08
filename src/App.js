233af1349fdc3d0e2e4f384a49d98f262b2599e5
rahul <rahulgoswami1520@gmail.com>
2023-09-08 10:29:15 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import "./style.css";
 
+let containerStyle = {
+  width: "90%",
+  height: "70vh",
+  border: "2px solid grey",
+  margin: "2% auto",
+  borderRadius: "20px",
+  display: "grid",
+  placeItems: "center",
+  backgroundColor: "#fcfcfc"
+}
+let backgroundColor="#fcfcfc";
+
+const colors = ["#fcfcfc","#F5F4F5","#f5f7f6","#d2d4d2","#909190","#727372","#474747"]
+
+const reducer = (state, action) => {
+  switch (action.type)
+  {
+    case "increase":
+      if (state < colors.length-1)
+      {
+        state = state + 1;
+        backgroundColor = colors[state];
+      }
+      else{
+        alert("darkest mode");
+      }
+      return state;
+    case "decrease":
+      if(state>0)
+      {
+        
+        state = state - 1;
+    
+        backgroundColor = colors[state];
+        
+      }
+      else{
+      
+        alert("lightest mode")
+      }  
+      return state;
+    default:
+      return state;
+  }
+}
 function App() {
+  
+  const [state, dispatch] = useReducer(reducer, 0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" style={{...containerStyle,backgroundColor}}>
+      <div id="controls">
+        <i class="fa-solid fa-minus" onClick={() => dispatch({ type: "decrease" })}></i>
+        <i class="fa-solid fa-plus" onClick={() => dispatch({ type: "increase" })}></i>
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
